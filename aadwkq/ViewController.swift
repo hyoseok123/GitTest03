2019453df8eebfc58d39faf9c9d32407a70cd312
dit06 <dit@imac-5.local>
2019-03-05 22:48:11 -0800

    Initial Commit

---
+++ aadwkq/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aadwkq
+//
+//  Created by dit06 on 2019. 3. 5..
+//  Copyright © 2019년 dit06. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
